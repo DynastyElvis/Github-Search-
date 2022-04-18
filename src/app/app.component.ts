c4ee2c854bb8845513e1f8b333971dcd1cba6554
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-18 09:48:27 +0300

    ft: added import features for modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RoutingModule } from './routing/routing.module'
+import { FormsModule } from '@angular/forms'
+// Defining routes
+// const routes:Routes=[
+//   {path:"users",component:UsersComponent},
+//   {path:"repos",component:ReposComponent},
+//   {path:"",redirectTo:"/users",pathMatch:"full"},
+//   {path:'**',component:NotFoundComponent}
+// ]
 
 @Component({
   selector: 'app-root',
