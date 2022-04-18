d61035a94a418a9c88d30b0bed1f40a366a793b7
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-18 10:42:24 +0300

    ft:font directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClassComponent } from './class/class.component';
+import { FontDirective } from './font.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClassComponent
+    ClassComponent,
+    FontDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/font.directive.ts
@@ -0,0 +1,11 @@
+import { Directive,ElementRef,Input,HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appFont]'
+
+})
+export class FontDirective {
+
+  constructor() { }
+
+}
