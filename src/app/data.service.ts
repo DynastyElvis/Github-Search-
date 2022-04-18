ce2a1d5e5d501741c70419a6d3546324a525fcee
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-18 12:07:39 +0300

    bg: logic update

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class DataService {
-
-  constructor() { }
-}
+import { Http, Headers } from '@angular/http';
+// import { HttpClient } from '@angular/common/http';
+import { Users } from '../class/users';
+import { Repos } from '../class/repos';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {environment} from '../../environments/environment'
