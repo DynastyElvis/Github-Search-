c7636e44e78ef5f5a8202003e160323a2df0b8f0
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-18 11:53:11 +0300

    bg: updated the logic of repos component

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -24,3 +24,28 @@ export class ReposComponent implements OnInit {
 
      
   }
+
+  searchRepo(){
+    this.dataService.updateRepos(this.reponame);
+    this.dataService.getRepoInfo().subscribe(repos => {
+     console.log(repos);
+     this.repos = repos;
+   });
+   
+  }
+
+  loadMore(){
+    // this.dataService.updateRepos(this.reponame);
+    this.dataService.updateShow(this.show);
+    this.dataService.getRepoInfo().subscribe(repos => {
+     console.log(repos);
+     this.repos = repos;
+   });
+   
+  }
+
+
+  ngOnInit() {
+  }
+
+}
